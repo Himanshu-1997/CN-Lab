f4fee869cb4387b55ae93b5e90bdca50f71360d4
Himanshu-1997 <himanshu28051997@gmail.com>
2017-08-11 11:14:31 +0530

    Create day4(node2).java

---
+++ day4(node2).java
@@ -0,0 +1,38 @@
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lab7
+ */
+public class Node2 {
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        DatagramSocket clientSocket = new DatagramSocket();	
+	while(true){
+            byte[] sendData = new byte[1024];
+            sendData = input.nextLine().getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("localhost"), 1080);
+            clientSocket.send(sendPacket);
+            byte[] inData = new byte[256];
+            DatagramPacket recievePacket = new DatagramPacket(inData, inData.length);
+            clientSocket.receive(recievePacket);
+            String msgrecieved = new String(recievePacket.getData());
+            System.out.println("RECIEVED : " + msgrecieved);
+            if(msgrecieved.contains("Timestamp"))
+                break;
+        }
+        clientSocket.close();
+    }
+}
+
